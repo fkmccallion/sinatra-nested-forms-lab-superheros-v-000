50ef2c2debd38811174ff04560a84dcd2f602358
Frank McCallion <fkmccallion@gmail.com>
2018-04-24 13:23:36 +0000

    automatically backed up by learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,10 +2,16 @@ class Hero
 
   attr_accessor :name, :power, :bio
 
+  @@all = []
+
   def initialize(params)
     @name = params[:name]
     @power = params[:power]
     @bio = params[:bio]
+  end
+
+  def self.all
+    @@all
   end
 
 
